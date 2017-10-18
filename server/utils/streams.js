0594619e8c6b898f5aed1ae82300505ecc8075dd
Ihar Yachnik <jutigor@yandex.ru>
2017-10-18 11:36:35 +0300

    Add bundle action

--- server/utils/streams.js
+++ server/utils/streams.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const promisify = require('util').promisify;
 
 const parser = require('nomnom');
 const through = require('through2');
 const split = require('split');
+const request = require('request');
+
+const readDir = promisify(fs.readdir);
 
 function run() {
-  const { action, file } = parser
+  const { action, file, path } = parser
     .option('action', {
       abbr: 'a',
       help: 'Name of action',
@@ -15,6 +19,10 @@ function run() {
     .option('file', {
       abbr: 'f',
       help: 'File path'
+    })
+    .option('path', {
+      abbr: 'p',
+      help: 'Directory path'
     })
     .parse();
 
@@ -29,10 +37,17 @@ function run() {
       case 'transform-file':
         transformFile(file);
         break;
+      case 'bundle-css':
+        bundle(path);
+        break;
     }
   } else {
     console.log('Usage: node streams.js [action] [options]');
   }
+}
+
+function help(param) {
+  console.log(`The '${param}' is required for this action`);
 }
 
 function createPipe(source, dest, cb) {
@@ -43,10 +58,14 @@ function createPipe(source, dest, cb) {
 }
 
 function inputOutput(filePath) {
+  if (!filePath) return help('file');
+
   createPipe(fs.createReadStream(filePath), process.stdout, getJsonConverter());
 }
 
 function transformFile(filePath) {
+  if (!filePath) return help('file');
+
   const filename = path.basename(filePath).split('.').shift();
   const destFileName = `${filename}.json`;
 
@@ -61,6 +80,58 @@ function transform() {
   });
 
   createPipe(process.stdin, process.stdout, converter);
+}
+
+function bundle(dirPath) {
+  if (!dirPath) return help('path');
+
+  const converter = () => through(function (chunk, enc, cb) {
+    console.log(chunk.toString());
+    this.push(`${chunk.toString()}\n`);
+
+    cb();
+  });
+
+  const flags = (() => {
+    const flags = { flags: 'a' };
+    let first = true;
+
+    return () => {
+      if (first) {
+        first = false;
+        return null;
+      } else {
+        return flags;
+      }
+    }
+  })();
+
+  function next(source, converter, flags) {
+    return new Promise((res, rej) => {
+      const dest = fs.createWriteStream('bundle.css', flags);
+
+      source.on('end', () => {
+        res();
+      });
+
+      source
+        .pipe(converter)
+        .pipe(dest);
+    });
+  }
+
+  readDir(dirPath)
+    .then(files => {
+      return files
+        .filter(file => path.extname(file) === '.css')
+        .map(file => getFilePath(dirPath, file))
+        .map(file => fs.createReadStream(file))
+        .concat(request('https://www.epam.com/etc/clientlibs/foundation/main.min.fc69c13add6eae57cd247a91c7e26a15.css'));
+    })
+    .then(streams => {
+      streams
+        .reduce((p, file) => p.then(() => next(file, converter(), flags())), Promise.resolve());
+    })
 }
 
 function getJsonConverter() {
@@ -86,6 +157,10 @@ function csvToJson(data) {
   const parsedData = data.split(/(".*")?,/g).filter(Boolean);
 
   return JSON.stringify(parsedData);
+}
+
+function getFilePath(dirPath, filePath) {
+  return path.resolve(dirPath, filePath);
 }
 
 if (__filename === process.argv[1]) {
