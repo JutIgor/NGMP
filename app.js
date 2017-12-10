15bf77e3f1457a4b558ea7db2801824c8bd2e035
Ihar Yachnik <jutigor@yandex.ru>
2017-12-10 19:51:54 +0300

    Add 8 task

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ import './auth';
 
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
+
 app.use(bodyParser.json());
 app.use(queryParser);
 app.use(cookieParser);
